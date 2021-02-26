a68893861fda195c4c684fe37128760f577728cf
Victor Dantas <victorodantas@gmail.com>
2021-02-25 21:04:13 -0300

    iniciando a atividade 2 jpa

---
+++ src/test/java/com/example/jpafc/JpafcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpafc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpafcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
